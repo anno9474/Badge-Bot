f27a15a9174037e75e738e79f01d6045677f2f8f
Anno9474 <anno9474@gmail.com>
2023-10-13 14:38:59 +0200

    use chat gpts version

--- src/commands/fun/tictactoe.js
+++ src/commands/fun/tictactoe.js
@@ -1,35 +1,27 @@
-const { SlashCommandBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder} = require('discord.js');
+const { SlashCommandBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('tictactoe')
-        .setDescription('A simple Tic Tac Toe Game for two player'),
+        .setDescription('A simple Tic Tac Toe Game for two players'),
     async execute(interaction) {
-
-        let buttonsArray = [];
+        let actionRows = [];
 
         for (let i = 0; i < 3; i++) {
-            let rowButtons = [];
+            const row = new ActionRowBuilder();
             for (let j = 0; j < 3; j++) {
                 const button = new ButtonBuilder()
                     .setCustomId(`tic_${i}_${j}`)
                     .setLabel('-')
                     .setStyle(ButtonStyle.Secondary);
-                rowButtons.push(button);
+                row.addComponents(button);
             }
-            buttonsArray.push(rowButtons);
+            actionRows.push(row);
         }
-
-        const firstRow = new ActionRowBuilder()
-            .addComponents(buttonsArray[0], buttonsArray[1], buttonsArray[2]);
-        const secondRow = new ActionRowBuilder()
-            .addComponents(buttonsArray[3], buttonsArray[4], buttonsArray[5]);
-        const thirdRow = new ActionRowBuilder()
-            .addComponents(buttonsArray[6], buttonsArray[7], buttonsArray[8]);
 
         await interaction.reply({
             content: "Let's Play!",
-            components: [firstRow, secondRow, thirdRow],
+            components: actionRows,
         });
     },
 };
