b45c1b0ec4bbd3c05caa783c77dd03cfefb4cf96
Anno9474 <anno9474@gmail.com>
2023-10-13 12:20:13 +0200

    make commands available globaly and remove guild id

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId } = require('./config.json');
+const { clientId } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -35,7 +35,7 @@ const rest = new REST().setToken(process.env.TOKEN);
 
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
+            Routes.applicationCommands(clientId),
             { body: commands },
         );
 
