4a2c5125c4f2b70a5563492c606d5a679d395d4a
Anno9474 <anno9474@gmail.com>
2023-10-15 21:18:55 +0200

    add new selector and move some files!

---
+++ src/commands/learning/selector.js
@@ -0,0 +1,34 @@
+const { StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('gender')
+        .setDescription('Pick what you want!'),
+    async execute(interaction) {
+        const select = new StringSelectMenuBuilder()
+            .setCustomId('gender')
+            .setPlaceholder('Tell me your Gender!')
+            .addOptions(
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Male')
+                    .setDescription('Male by birth or by choice')
+                    .setValue('male'),
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Female')
+                    .setDescription('Female by birth or by choice')
+                    .setValue('female'),
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('custom')
+                    .setDescription('Human being, all I need to know')
+                    .setValue('custom'),
+            );
+
+        const row = new ActionRowBuilder()
+            .addComponents(select);
+
+        await interaction.reply({
+            content: 'Choose your starter!',
+            components: [row],
+        });
+    },
+};
